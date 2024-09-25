b7fe435e12b078ad6d8b323dc69d3b3bf390e8e5
Say something <163742930+Raks-kmt@users.noreply.github.com>
2024-09-25 09:50:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ from os import getenv
 
 API_ID = int(getenv("API_ID", "28526237"))
 API_HASH = getenv("API_HASH", "936db76a74f9a52cfb2cea8a62e4c20e")
-BOT_TOKEN = getenv("BOT_TOKEN", "7450435842:AAGHFbVV0TGbiSBfLnlmcSOn8OrE3voFwa0")
+BOT_TOKEN = getenv("BOT_TOKEN", "7450435842:AAGH-o4EDMYTQoeewcTEOEJWy91hEW6DtXc")
 OWNER_ID = int(getenv("OWNER_ID", "6486192717"))
 MONGODB_CONNECTION_STRING = getenv("MONGO_DB", "mongodb+srv://prisarax:WxuKANYzL5CPyY7d@cluster0.jxlwo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 LOG_GROUP = int(getenv("LOG_GROUP", "-1002204974717"))
